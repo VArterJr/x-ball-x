7ff51eb4a23285dcb53ec31ce83ace8857a8ef6b
Vince Arter, Jr <vince@arter.us>
2023-11-19 14:38:41 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,4 @@
+let mySprite: Sprite = null
+let screenWidth = scene.screenWidth()
+let screenHeight = scene.screenHeight()
+tiles.placeOnTile(mySprite, tiles.getTileLocation(randint(0, screenWidth), randint(0, screenHeight)))
